0829c659b2d654c83238f13a92b04651f9bb825a
James Briggs <jfbriggs106@gmail.com>
2021-03-30 15:30:47 -0700

    Add routes and functions

--- main.py
+++ main.py
@@ -1,9 +1,58 @@
 from fastapi import FastAPI
-from typing import Dict
+from typing import Dict, Optional
+from enum import Enum
+
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
 
 
 @app.get("/")
 async def root() -> Dict[str, str]:
     return {"message": "Hello World"}
+
+
+@app.post("/items/")
+async def create_item(item: Item) -> dict:
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+
+@app.put("/items/{item_id}")
+async def create_item(item_id: int, item: Item, q: Optional[str] = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int) -> Dict[str, int]:
+    return {"item_id": item_id}
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
